21e23dd2d4454c420db2c17ecedbc4668983f2e4
HalaBarbary <hala.elbarbary@gmail.com>
2023-10-09 14:59:28 -0700

    print the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - C program entry point
+ * Description: C program entry point
+ * Return: 0 if the program runs correctly
+*/
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet != 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('z');
+	return (0);
+}
