115487b1f3ada2addb1d654042aec870f7644b1c
HalaBarbary <hala.elbarbary@gmail.com>
2023-10-09 16:07:31 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - C program entry point
+ * Description: C program entry point
+ * Return: 0 if the program runs correctly
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d", x);
+	}
+	return (0);
+}
