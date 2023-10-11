ad01868393b70ec35846039ca01c8a06900f3aca
HalaBarbary <hala.elbarbary@gmail.com>
2023-10-10 20:37:58 -0700

    print the alphabet function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - C program entry point
+ * Description: C program entry point
+ * Return: 0 if the program runs correctly
+*/
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,3 +8,28 @@ int pprint (void)
 	printf("_putchar\n");
 	return (0);
 }
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+#include <unistd.h>
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/* print_alphabet - prints the alphabet in lowercase followed by a newline*/
+void print_alphabet(void);
+
+void print_alphabet(void)
+{
+	char x;
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
