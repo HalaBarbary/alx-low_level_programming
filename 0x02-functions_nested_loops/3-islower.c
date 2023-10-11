5da91e12bccbfbf62df76f3b2d4ca0d5cfa084ae
HalaBarbary <hala.elbarbary@gmail.com>
2023-10-11 13:09:45 -0700

    check character case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - checks lowercase
+ * Description: checks charcter case depending on ascii
+ * @c: for storing ascii
+ * Return: return 1 if lower case and 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,3 +12,8 @@ void print_alphabet(void);
  */
 
 void print_alphabet_x10(void);
+
+/**
+ * _islower - checks if a character is lowercase
+ */
+int _islower(int c);
