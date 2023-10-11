b6bee38fd2fd65fb59cb068e406a98583e53e259
HalaBarbary <hala.elbarbary@gmail.com>
2023-10-11 13:47:51 -0700

    chek sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,17 +7,20 @@
  */
 int print_sign(int n)
 {
+	int ret_val;
+
 	if (n > 0)
 	{
 		_putchar('+');
-		return (1);
+		ret_val = 1;
 	} else if (n < 0)
 	{
 		_putchar('-');
-		return (-1);
+		ret_val = -1;
 	} else if (n == 0)
 	{
 		_putchar('0');
-		return (0);
+		ret_val = 0;
 	}
+	return (ret_val);
 }
