b856cfbbaca7b0db2112250a579406c6bb600b26
HalaBarbary <hala.elbarbary@gmail.com>
2023-10-15 13:57:23 -0700

    Jack Bauer Day in minutes

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,20 +6,20 @@
  */
 void jack_bauer(void)
 {
-	int h;
-	int m;
+	char h;
+	char m;
 
-	for (h = 0; h <= 24; h++)
+	for (h = '0'; h < '24'; h++)
 	{
-		for (m = 0; m < 60; m++)
+		for (m = '0'; m < '60'; m++)
 		{
 			if (h < 10)
 			{
 				_putchar('0');
-				_putchar((int)h);
+				_putchar(h);
 			} else
 			{
-				_putchar((int)h);
+				_putchar(h);
 			}
 
 			_putchar(':');
@@ -27,11 +27,11 @@ void jack_bauer(void)
 			if (m < 10)
 			{
 				_putchar('0');
-				_putchar((int)m);
+				_putchar(m);
 				_putchar('\n');
 			} else
 			{
-				_putchar((int)m);
+				_putchar(m);
 				_putchar('\n');
 			}
 		}
