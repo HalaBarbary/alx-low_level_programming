0d7cf73389f4b1e0e385545439b3c87fb9464473
HalaBarbary <hala.elbarbary@gmail.com>
2023-10-19 21:53:47 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - resets the value pointed at to 98
+ * @n: a pointer to an iteger
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+/* _putchar */
+int _putchar(char c);
+
+/* reset to 98*/
+void reset_to_98(int *n);
+
