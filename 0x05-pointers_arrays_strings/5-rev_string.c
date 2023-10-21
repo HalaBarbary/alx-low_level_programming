d2a93c1af6fef1b1648d214d796128e988aafbd0
HalaBarbary <hala.elbarbary@gmail.com>
2023-10-21 14:34:55 -0700

    reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - reverse a string
+ *@s: a pointer to a string
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	char str[];
+	int i;
+	int x;
+
+	for (i = 0; *s != 0, i++)
+	{
+		s++;
+	}
+
+	for (x = 0, x <= i, x++)
+	{
+		str[x] = *s;
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -15,3 +15,18 @@ void _puts(char *str);
 
 /*print a reversed string*/
 void print_rev(char *s);
+
+/* reverse a string*/
+void rev_string(char *s);
+
+/*print every other char*/
+void puts2(char *str);
+
+/*print half a string*/
+void puts_half(char *str);
+
+/*print array elements*/
+void print_array(int *a, int n);
+
+/*copies a string into another*/
+char *_strcpy(char *dest, char *src);
