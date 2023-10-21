e8550e602c55e794e703c07195b02efdec325607
HalaBarbary <hala.elbarbary@gmail.com>
2023-10-21 14:44:52 -0700

    reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,17 +7,16 @@
 
 void rev_string(char *s)
 {
-	char str[];
 	int i;
 	int x;
 
-	for (i = 0; *s != 0, i++)
+	for (i = 0, *s != 0, i++)
 	{
 		s++;
 	}
 
 	for (x = 0, x <= i, x++)
 	{
-		str[x] = *s;
+		char str[x] = *s;
 	}
 }
