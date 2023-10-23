1e8d77fa46ce9aa46f37a1ea261c45a9beaafc64
HalaBarbary <hala.elbarbary@gmail.com>
2023-10-23 15:16:31 -0700

    search a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,18 +9,13 @@
 char *_strchr(char *s, char c)
 {
 	unsigned long int i;
-	char *h;
 
 	for (i = 0; i < sizeof(s); i++)
 	{
 		if (s[i] == c)
 		{
-			h = &s[i];
-		}
-		else
-		{
-			h = '\0';
+			return (s + i);
 		}
 	}
-	return (h);
+	return ('\0');
 }
