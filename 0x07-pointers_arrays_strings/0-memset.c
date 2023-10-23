3d57d359464b90f9e5d0abd95d9164f09f9ab712
HalaBarbary <hala.elbarbary@gmail.com>
2023-10-23 13:31:46 -0700

    memory setting

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,10 +10,11 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
-		*s = b;
+		*s = b;	
 		s++;
 	}
+
 	return (s);
 }
