0fd620e091f3b7b1bbce757ec84f069a6548e683
HalaBarbary <hala.elbarbary@gmail.com>
2023-10-23 13:11:09 -0700

    memory setting

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset -sets a block of memory to a constant
+ * @s: pointer to achracter loctaes the memory block
+ * @b: constant char to be assigned
+ * @n: number of bytes set
+ * Return: pointer to char s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0, i <= n, i++)
+	{
+		*s = b;
+		s++;
+	}
+	return (s);
+}
