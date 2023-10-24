cd1af3aa508d2d3c3676334735751ca0f6897ca5
HalaBarbary <hala.elbarbary@gmail.com>
2023-10-24 12:55:20 -0700

    copy a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcpy - copies a string into another
+ * @dest: destination string
+ * @src: source string
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] >= '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
