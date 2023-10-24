2b68b6cb596aa8ed6262d2ca320e06c3aec4adb7
HalaBarbary <hala.elbarbary@gmail.com>
2023-10-24 12:37:21 -0700

    print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,10 +14,10 @@ void print_array(int *a, int n)
 	{
 		if (i != n - 1)
 		{
-			printf("%p, ", *a[i]);
+			printf("%p, ", a[i]);
 		} else
 		{
-			printf("%p ", *a[i]);
+			printf("%p ", a[i]);
 		}
 
 	}
