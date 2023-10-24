df8f94c7f1730af98e4c028c5d342fa01c9bad00
HalaBarbary <hala.elbarbary@gmail.com>
2023-10-24 13:27:59 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print from 0 to 9
+ * Return: nothing
+ */
+void print_numbers(void)
+{
+	int i, x;
+
+	x = 48;
+	for (i = 0; x < = 57; i++)
+	{
+		_putchar(x);
+		x++;
+	}
+}
