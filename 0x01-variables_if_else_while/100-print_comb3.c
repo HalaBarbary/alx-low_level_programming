75633df2e27f51257cab36fc9085dc9a7a5f9265
HalaBarbary <hala.elbarbary@gmail.com>
2023-10-24 15:02:50 -0700

    advnced

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - C program entry point
+ * Description: C program entry point
+ * Return: 0 if the program runs correctly
+ */
+int main(void)
+{
+	int i;
+	int x;
+
+	for (i = 48; i <= 56; i++)
+	{
+		for (x = 49; x <= 57; x++)
+		{
+			putchar(i);
+			putchar(x);
+			if (i == 56 && x == 57)
+			{
+				purchar(',');
+				putchar(' ');
+			}
+				putchar(' ');
+		}
+	}
+	return (0);
+}
