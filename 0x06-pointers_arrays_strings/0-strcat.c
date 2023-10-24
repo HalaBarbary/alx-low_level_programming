f8e76b37866c3f91c6cbffc6af6d847b93e1c128
HalaBarbary <hala.elbarbary@gmail.com>
2023-10-24 15:31:07 -0700

    cat 2 strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcat - concatenates 2 strings
+ * @dest: pointer to setination string
+ * @src: pointer to source
+ * Return: pointer to destination
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int x;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	for (x = 0; src[x] != '\0'; x++)
+	{
+		dest[i] = src[x];
+		i++;
+	}
+	dest[i] = '\0'
+	return (dest);
+}
+
