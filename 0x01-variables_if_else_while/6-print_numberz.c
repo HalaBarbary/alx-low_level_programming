8449f518101c69e202f1ca16f3261a74642822ef
HalaBarbary <hala.elbarbary@gmail.com>
2023-10-24 14:11:55 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - C program entry point
+ * Description: C program entry point
+ * Return: 0 if the program runs correctly
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
