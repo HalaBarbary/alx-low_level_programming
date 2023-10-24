8e4d5ed198adcca218a554e32416d65e67f8a9db
HalaBarbary <hala.elbarbary@gmail.com>
2023-10-24 14:19:26 -0700

    print reverse lowercase

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - C program entry point
+ * Description: C program entry point
+ * Return: 0 if the program runs correctly
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i >= 97; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
