c79d8cef49e6c45a9f2ee359e49f8fc0e72e5370
HalaBarbary <hala.elbarbary@gmail.com>
2023-10-26 09:34:52 -0700

    print reversed string recursively

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -9,14 +9,15 @@ void _print_rev_recursion(char *s)
 	char *r;
 
 	r = s;
+
 	if (*s != '\0')
 	{
 		s++;
 		_print_rev_recursion(s);
-	} else if (s != r)
+	} 
+	if ( s != r)
 	{
 		s--;
 		_putchar(*s);
-		_print_rev_recursion(s);
 	}
 }
