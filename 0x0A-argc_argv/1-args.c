6565733b90cdd0862328b57b7e78dca382b694e5
HalaBarbary <hala.elbarbary@gmail.com>
2023-10-29 09:29:59 -0700

    print number of main arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - C program entry point
+ * @argc: arguments count
+ * @argv: a pointer to a string
+ * Return: 0 if the program runs correctly
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
