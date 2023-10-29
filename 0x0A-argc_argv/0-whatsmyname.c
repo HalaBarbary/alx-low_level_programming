5c5cda7bbad56a2f2197bfc38e773cada15bc46c
HalaBarbary <hala.elbarbary@gmail.com>
2023-10-29 08:38:56 -0700

    print program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - C program entry point
+ * @argc: integer for counting arguments
+ * @argv: an array of chars to save the arguments
+ * Return: 0 if the program runs correctly
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
