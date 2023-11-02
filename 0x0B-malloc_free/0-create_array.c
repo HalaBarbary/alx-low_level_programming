4245536e88a8c2bc3e407348140a95f76b112aac
HalaBarbary <hala.elbarbary@gmail.com>
2023-11-02 12:55:05 -0700

    malloc first

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,11 +8,16 @@
  */
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
+
 	if (size > 0)
 	{
 		char *a = malloc(size);
-
-		a[0] = c;
+		
+		for (i = 0; i < size; i++)
+		{	
+		a[i] = c;
+		}
 		return (a);
 	}
 		return ('\0');
